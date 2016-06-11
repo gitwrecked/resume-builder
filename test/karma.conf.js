be606bce103206f6de4cc84e7bb0edc541df2177
Matt Hoang <Matt Hoang>
2016-06-11 17:46:06 +0100

    initial commit for about page

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -1,20 +1,20 @@
 // Karma configuration
 // Generated on Mon Jun 06 2016 14:36:16 GMT-0500 (CDT)
 
-module.exports = function(config) {
-  config.set({
+module.exports = function (config) {
+    config.set({
 
-    // base path that will be used to resolve all patterns (eg. files, exclude)
-    basePath: '',
-
-
-    // frameworks to use
-    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['jasmine'],
+        // base path that will be used to resolve all patterns (eg. files, exclude)
+        basePath: '',
 
 
-    // list of files / patterns to load in the browser
-    files: [
+        // frameworks to use
+        // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+        frameworks: ['jasmine'],
+
+
+        // list of files / patterns to load in the browser
+        files: [
       '../app/lib/angular/angular.js',
       '../app/lib/angular-mocks/angular-mocks.js',
       '../app/*.js',
@@ -26,62 +26,61 @@ module.exports = function(config) {
     ],
 
 
-    // list of files to exclude
-    exclude: [
+        // list of files to exclude
+        exclude: [
     ],
 
 
-    // preprocess matching files before serving them to the browser
-    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-    preprocessors: {
-    },
+        // preprocess matching files before serving them to the browser
+        // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+        preprocessors: {},
 
 
-    // test results reporter to use
-    // possible values: 'dots', 'progress'
-    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress','html'],
+        // test results reporter to use
+        // possible values: 'dots', 'progress'
+        // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+        reporters: ['progress', 'html'],
 
-    htmlReporter: {
-      outputDir: 'test', // where to put the reports  
-      templatePath: null, // set if you moved jasmine_template.html 
-      focusOnFailures: true, // reports show failures on start 
-      namedFiles: false, // name files instead of creating sub-directories 
-      pageTitle: null, // page title for reports; browser info by default 
-      urlFriendlyName: false, // simply replaces spaces with _ for files/dirs 
-      reportName: 'reports', // report summary filename; browser info by default 
-      // experimental 
-      preserveDescribeNesting: false, // folded suites stay folded  
-      foldAll: false, // reports start folded (only with preserveDescribeNesting) 
-    },
-    // web server port
-    port: 9876,
-
-
-    // enable / disable colors in the output (reporters and logs)
-    colors: true,
+        htmlReporter: {
+            outputDir: 'test', // where to put the reports  
+            templatePath: null, // set if you moved jasmine_template.html 
+            focusOnFailures: true, // reports show failures on start 
+            namedFiles: false, // name files instead of creating sub-directories 
+            pageTitle: null, // page title for reports; browser info by default 
+            urlFriendlyName: false, // simply replaces spaces with _ for files/dirs 
+            reportName: 'reports', // report summary filename; browser info by default 
+            // experimental 
+            preserveDescribeNesting: false, // folded suites stay folded  
+            foldAll: false, // reports start folded (only with preserveDescribeNesting) 
+        },
+        // web server port
+        port: 9876,
 
 
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_INFO,
+        // enable / disable colors in the output (reporters and logs)
+        colors: true,
 
 
-    // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: true,
+        // level of logging
+        // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+        logLevel: config.LOG_INFO,
 
 
-    // start these browsers
-    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['PhantomJS'],
+        // enable / disable watching file and executing tests whenever any file changes
+        autoWatch: true,
 
 
-    // Continuous Integration mode
-    // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false,
+        // start these browsers
+        // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+        browsers: ['PhantomJS'],
 
-    // Concurrency level
-    // how many browser should be started simultaneous
-    concurrency: Infinity
-  });
+
+        // Continuous Integration mode
+        // if true, Karma captures browsers, runs the tests and exits
+        singleRun: false,
+
+        // Concurrency level
+        // how many browser should be started simultaneous
+        concurrency: Infinity
+    });
 }
